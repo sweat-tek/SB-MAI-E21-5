e0706c29b790ecbd4af99dc39556d929103b8d05
Anton Vibæk <55436877+Svendpai@users.noreply.github.com>
2021-11-05 18:27:03 +0100

    small restructure of createFontButton method

--- JHotDraw/src/main/java/org/jhotdraw/draw/action/ButtonFactory.java
+++ JHotDraw/src/main/java/org/jhotdraw/draw/action/ButtonFactory.java
@@ -1283,13 +1283,9 @@ public class ButtonFactory {
         return createFontButton(editor, FONT_FACE, labels);
     }
 
-    public static JPopupButton createFontButton(DrawingEditor editor,
-            AttributeKey<Font> key,
-            ResourceBundleUtil labels) {
+    public static JPopupButton createFontButton(DrawingEditor editor, AttributeKey<Font> key, ResourceBundleUtil labels) {
 
-        JPopupButton fontPopupButton;
-
-        fontPopupButton = new JPopupButton();
+        JPopupButton fontPopupButton = new JPopupButton();
 
         labels.configureToolBarButton(fontPopupButton, "attribute.font");
         fontPopupButton.setFocusable(false);
