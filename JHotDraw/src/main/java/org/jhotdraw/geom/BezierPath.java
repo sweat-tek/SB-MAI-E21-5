2d7fd25dc4c9f08cbe9302f65f70a49f7c8b3ab7
jaco5157 <jacob-dan@hotmail.com>
2021-11-10 11:13:52 +0100

    feat: Create inner class Coords

--- JHotDraw/src/main/java/org/jhotdraw/geom/BezierPath.java
+++ JHotDraw/src/main/java/org/jhotdraw/geom/BezierPath.java
@@ -473,6 +473,86 @@ public class BezierPath extends ArrayList<BezierPath.Node>
         return generalPath.intersects(x, y, w, h);
     }
 
+    public class Coords {
+        
+        private double x;
+        private double y;
+        private double x1;
+        private double y1;
+        private double x2;
+        private double y2;
+        
+        
+        public Coords(double x1, double y1, double x2, double y2){
+            this.x = 0;
+            this.y = 0;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public Coords(double x, double y, double x1, double y1, double x2, double y2){
+            this.x = x;
+            this.y = y;
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        
+        public double getX() {
+            return x;
+        }
+        public void setX(double x) {
+            this.x = x;
+        }
+        public double getY() {
+            return y;
+        }
+        public void setY(double y) {
+            this.y = y;
+        }
+        public double getX1() {
+            return x1;
+        }
+        public void setX1(double x1) {
+            this.x1 = x1;
+        }
+        public double getY1() {
+            return y1;
+        }
+        public void setY1(double y1) {
+            this.y1 = y1;
+        }
+        public double getX2() {
+            return x2;
+        }
+        public void setX2(double x2) {
+            this.x2 = x2;
+        }
+        public double getY2() {
+            return y2;
+        }
+        public void setY2(double y2) {
+            this.y2 = y2;
+        }
+    }
+    
+    public void handleBounds(Coords coords){
+        if (coords.getX() < coords.getX1()) {
+            coords.setX1(coords.getX());
+        }
+        if (coords.getY() < coords.getY1()) {
+            coords.setY1(coords.getY());
+        }
+        if (coords.getX() > coords.getX2()) {
+            coords.setX2(coords.getX());
+        }
+        if (coords.getY() > coords.getY2()) {
+            coords.setY2(coords.getY());
+        }
+    }
+    
     public Rectangle2D.Double getBounds2D() {
         if (bounds == null) {
             double x1, y1, x2, y2;
