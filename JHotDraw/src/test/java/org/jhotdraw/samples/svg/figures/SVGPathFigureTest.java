9e18594fe5e5012cc28f63223d3b8be70e0d44d3
anton <antontj@live.dk>
2021-11-10 15:27:10 +0100

    Add unit tests

---
+++ JHotDraw/src/test/java/org/jhotdraw/samples/svg/figures/SVGPathFigureTest.java
@@ -0,0 +1,51 @@
+package org.jhotdraw.samples.svg.figures;
+
+import java.awt.geom.Rectangle2D;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author antje12
+ */
+public class SVGPathFigureTest {
+    
+    public SVGPathFigureTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getDrawingArea method, of class SVGPathFigure.
+     */
+    @Test
+    public void testGetDrawingArea() {
+        SVGPathFigure instance = new SVGPathFigure();
+        
+        Rectangle2D.Double expResult = new Rectangle2D.Double(-2, -2, 4, 4);
+        Rectangle2D.Double result = instance.getDrawingArea();
+        
+        assertEquals(expResult.x, result.x, 0);
+        assertEquals(expResult.y, result.y, 0);
+        assertEquals(expResult.width, result.width, 0);
+        assertEquals(expResult.height, result.height, 0);
+    }
+}
